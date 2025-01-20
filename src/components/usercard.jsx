7f23017f2c503152aad29f592f069b2ca6f884c4
Isaac Reji <isaac.reji@kalvium.community>
2025-01-20 15:41:06 +0530

    assignment completed

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,26 @@
 // write the component code here
+import React from 'react'
+import profilePhoto from '../assets/download.png';
+function Usercard() {
+    const user={
+        name:"John Doe",
+        mail:"johndoe@gmail.com",
+        phoneNumber:"9552152XXX",
+        address:"54 bleecker street"
+    }
+  return (
+    <div>
+        <img src={profilePhoto} alt="Profile" style={{ width: '100px', borderRadius: '50%' }} />
+        <br></br>
+        {user.name}
+        <br></br>
+        {user.mail}
+        <br></br>
+        {user.phoneNumber}
+        <br></br>
+        {user.address}
+    </div>
+  )
+}
+
+export default Usercard
